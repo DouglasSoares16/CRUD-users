904ccb11c55a09d3a2b98a5e1148eab8884f7b68
Douglas Soares <douglasmaismcpe@gmail.com>
2021-11-29 08:58:08 -0300

    added database config

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "mysql",
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: "crud_users",
+  define: {
+    timestamp: true,
+    underscored: true
+  }
+};
