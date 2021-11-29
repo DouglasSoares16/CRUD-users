17c5c15c408232ef98313e13823733ce6619402b
Douglas Soares <douglasmaismcpe@gmail.com>
2021-11-29 09:25:45 -0300

    added and configured handlebars

---
+++ src/config/handlebars.js
@@ -0,0 +1,7 @@
+module.exports = {
+  defaultLayout: "main",
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true,
+  },
+};
--- src/server.js
+++ src/server.js
@@ -2,8 +2,16 @@ require("./database/connection");
 
 const express = require("express");
 const routes = require("./routes");
+const hbsConfig = require("./config/handlebars");
+
+const { engine } = require("express-handlebars");
+const { join } = require("path");
 
 const app = express();
+
+app.engine("handlebars", engine(hbsConfig));
+app.set("view engine", "handlebars");
+app.set("views", join(__dirname, "views"));
 
 app.use(routes);
 
