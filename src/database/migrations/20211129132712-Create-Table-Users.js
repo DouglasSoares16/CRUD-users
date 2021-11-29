7d4c574853767b1178c8b3f7851aa9d75a5bcc98
Douglas Soares <douglasmaismcpe@gmail.com>
2021-11-29 10:37:15 -0300

    created table users

---
+++ src/database/migrations/20211129132712-Create-Table-Users.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      country: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  }
+};
